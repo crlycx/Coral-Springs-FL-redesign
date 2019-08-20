ffd726dcac059dc3abbae82ec5be38dd9e5662eb
Carly Cox <webdevbarbie@gmail.com>
2019-08-20 11:56:09 -0500

    design homepage buttons, beginning html/css for cards

---
+++ index.js
@@ -0,0 +1,13 @@
+// function openMenu() {
+//   // Get the checkbox
+//   var checkBox = document.getElementById("menu");
+//   // Get the div with menu ul
+//   var menu = document.getElementById("menu-content");
+//
+//   // If the checkbox is checked, display the div
+//   if (checkBox.checked == true) {
+//     menu.style.display = "block";
+//   } else {
+//     menu.style.display = "none";
+//   }
+// }
